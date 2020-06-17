32746a4680f37402d773df146a2f8659adcf4fd1
ngoc <ngocqh0601@gmail.com>
2020-06-17 09:21:23 +0700

    spa-template

--- UI/spa-template/js/main.js
+++ UI/spa-template/js/main.js
@@ -70,17 +70,6 @@
         }
     });
 
-    /*---------booking date picker---------*/
-    $("#booking-date").datepicker({
-        dateFormat: 'dd - mm - yy',
-        minDate: "new Date()",
-    });
-    // $("#booking-date2").daterangepicker();
-
-    $("#booking-date2").datepicker({
-        dateFormat: 'dd - mm - yy',
-        minDate: "new Date()",
-    });
 
 
     //----------product gallery-----------
