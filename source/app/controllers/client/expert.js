8f2d75cd9df6ab9d769976c5f5a517f025602000
danhtan <trieutandanh1997@gmail.com>
2020-07-01 16:22:34 +0700

    update expert service

--- source/app/controllers/client/expert.js
+++ source/app/controllers/client/expert.js
@@ -12,9 +12,7 @@ const index = async (req, res) => {
 
 const view = async (req, res) => {
   let slug = req.params.slug
-  let expert = await Expert.findOne({slug}).populate('user').exec();
-  console.log('expert', expert);
-  console.log('expert', expert.user);
+  let expert = await Expert.findOne({slug}).populate('user').populate('services').exec();
   res.render("client/experts/view", {
     expert,
     urlMediaUpload
